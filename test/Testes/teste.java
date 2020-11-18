0f0b97e3ed48eac6fb2ef01eb0d2ff9b2316380a
Luan Grabher <lujetix@hotmail.com>
2020-11-18 17:41:12 -0300

    Only change test file

--- test/Testes/teste.java
+++ test/Testes/teste.java
@@ -8,13 +8,13 @@ public class teste {
     public static void main(String[] args) {
         Integer enterprise = 729;
         Integer account = 148;
-        Integer participant = null;
+        Integer participant = 327896;
         Calendar startDate = Calendar.getInstance();
         Calendar endDate = Calendar.getInstance();
-        Boolean reworkConciliate = true;
+        Boolean reworkConciliate = true;        
 
         startDate.set(2020, 0, 1);
-        endDate.set(2020, 9, 31);
+        endDate.set(2020, 11, 31);
 
         System.out.println(
                 Conciliate.principal(enterprise, account, participant, startDate, endDate, reworkConciliate)
