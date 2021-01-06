fdef981895e0ddfd5e832dab752d37c71b3de79e
Admnistrador <Admnistrador@192.168.0.31>
2021-01-06 09:37:28 -0300

    update tests

--- test/Testes/teste.java
+++ test/Testes/teste.java
@@ -49,15 +49,15 @@ public class teste {
     }
     
     public static void test() {
-        Integer enterprise = 21;
-        Integer account = 963;
-        Integer participant = null;
+        Integer enterprise = 331;
+        Integer account = 148;
+        Integer participant = 872729;
         Calendar startDate = Calendar.getInstance();
         Calendar endDate = Calendar.getInstance();
-        Boolean reworkConciliate = true;
+        Boolean reworkConciliate = false;
 
-        startDate.set(2017, 0, 1);
-        endDate.set(2020, 10, 30);
+        startDate.set(2020, 0, 1);
+        endDate.set(2020, 11, 31);
 
         System.out.println(
                 Conciliate.principal(enterprise, account, participant, startDate, endDate, reworkConciliate)
