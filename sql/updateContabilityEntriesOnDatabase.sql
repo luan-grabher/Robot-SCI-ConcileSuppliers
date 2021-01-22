8517c4c63f90efe6e6263afcde00a650c2169ce5
Luan Grabher <lujetix@hotmail.com>
2021-01-22 08:53:25 -0300

    Commented on sql to update entries date, debit, credit, p credit, p debit, history code and val. Now only complement, document and conciliate fields are updated to security.

--- sql/updateContabilityEntriesOnDatabase.sql
+++ sql/updateContabilityEntriesOnDatabase.sql
@@ -1,14 +1,14 @@
 UPDATE VSUC_EMPRESAS_TLAN L
 SET 
-BDDATA = ':date',
-BDDEBITO = :accountDebit,
-BDCREDITO = :accountCredit,
-BDCODTERCEIROD = :participantDebit,
-BDCODTERCEIROC = :participantCredit,
-BDCODHIST = :historyCode,
+/*BDDATA = ':date',*/
+/*BDDEBITO = :accountDebit,*/
+/*BDCREDITO = :accountCredit,*/
+/*BDCODTERCEIROD = :participantDebit,*/
+/*BDCODTERCEIROC = :participantCredit,*/
+/*BDCODHIST = :historyCode,*/
 BDCOMPL = ':complement',
 BDDCTO = ':document',
-BDVALOR = :value,
+/*BDVALOR = :value,*/
 BDCONCC = :conciliedCredit,
 BDCONCD = :conciliedDebit
 WHERE 
